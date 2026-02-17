0296f53191cea8abef2bb5f24b12641f93cc1150
96SLM <slmitchell2@pstcc.edu>
2026-02-17 09:11:39 -0500

    Additions: Created Task.kt data class following affirmations tutorial Removed: N/A

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -4,13 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.taskmanagerapp.ui.theme.TaskManagerAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +13,23 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TaskManagerAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+
             }
         }
     }
 }
-
+//main app
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskManagerApp() {
+
 }
-
-@Preview(showBackground = true)
+//task list style
 @Composable
-fun GreetingPreview() {
-    TaskManagerAppTheme {
-        Greeting("Android")
-    }
+fun TaskLine() {
+
+}
+//list item component
+@Composable
+fun TaskList() {
+
 }
---
+++ app/src/main/java/com/example/taskmanagerapp/model/Tasks.kt
@@ -0,0 +1,7 @@
+package com.example.taskmanagerapp.model
+
+import androidx.annotation.StringRes
+
+data class Tasks(
+    @StringRes val stringResourceId: Int
+)
