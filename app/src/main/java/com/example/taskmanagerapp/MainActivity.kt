3d0899e455628fb50ce59f557f1c1d87cafe9f01
96SLM <slmitchell2@pstcc.edu>
2026-02-17 10:12:30 -0500

    Additions: TaskList content moved to TaskLine, TaskList function filled in Removed: Syntax error found and fixed

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,24 +36,31 @@ fun TaskManagerApp() {
 }
 //task list style
 @Composable
-fun TaskLine() {
-
+fun TaskLine(tasks: Tasks, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Text(
+                text = LocalContext.current.getString(tasks.stringResourceId),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+    }
 }
 //list item component
-//@Composable
-//fun TaskList(task: Tasks, modifier: Modifier = Modifier) {
-//    Card(modifier = modifier) {
-//        Column {
-//            Text(
-//                text = LocalContext.current.getString(Tasks.stringResourceId),
-//                modifier = Modifier.padding(16.dp)
-//            )
-//        }
-//    }
-//}
+@Composable
+fun TaskList(taskList: List<Tasks>, modifier: Modifier = Modifier) {
+    LazyColumn(modifier = modifier) {
+        items(taskList) { tasks ->
+            TaskLine(
+                tasks = tasks,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
+}
 
-//@Preview
-//@Composable
-//fun TaskListPreview() {
-//    TaskList(Tasks(R.string.Task1))
-//}
+@Preview
+@Composable
+fun TaskLinePreview() {
+    TaskLine(Tasks(R.string.Task1))
+}
