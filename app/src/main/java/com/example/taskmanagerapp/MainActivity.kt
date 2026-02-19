dee2e486305c643ab183dbf07589676524a49fbd
96SLM <slmitchell2@pstcc.edu>
2026-02-19 09:06:34 -0500

    Current project preview will build Additions: TaskLinePreview -> tested all current test strings within a colum, Created previews for TaskList and TaskManagerApp functions,
    Removed:

--- app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
+++ app/src/main/java/com/example/taskmanagerapp/MainActivity.kt
@@ -5,16 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.taskmanagerapp.data.DataSource
 import com.example.taskmanagerapp.model.Tasks
 import com.example.taskmanagerapp.ui.theme.TaskManagerAppTheme
 
@@ -32,7 +42,22 @@ class MainActivity : ComponentActivity() {
 //main app
 @Composable
 fun TaskManagerApp() {
-
+    val layoutDirection = LocalLayoutDirection.current
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding()
+            .padding(
+                start = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateStartPadding(layoutDirection),
+                end = WindowInsets.safeDrawing.asPaddingValues()
+                    .calculateEndPadding(layoutDirection),
+            ),
+    ) {
+        TaskList(
+            taskList = DataSource().loadTasks(),
+        )
+    }
 }
 //task list style
 @Composable
@@ -41,7 +66,7 @@ fun TaskLine(tasks: Tasks, modifier: Modifier = Modifier) {
         Column {
             Text(
                 text = LocalContext.current.getString(tasks.stringResourceId),
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(8.dp)
             )
         }
     }
@@ -53,14 +78,29 @@ fun TaskList(taskList: List<Tasks>, modifier: Modifier = Modifier) {
         items(taskList) { tasks ->
             TaskLine(
                 tasks = tasks,
-                modifier = Modifier.padding(8.dp)
+                modifier = Modifier.padding(4.dp)
             )
         }
     }
 }
-
 @Preview
 @Composable
-fun TaskLinePreview() {
-    TaskLine(Tasks(R.string.Task1))
+fun TaskManagerAppPreview() {
+    TaskManagerApp()
 }
+
+//@Preview
+//@Composable
+//fun TaskListPreview() {
+//    TaskList()
+//}
+
+//@Preview
+//@Composable
+//fun TaskLinePreview() {
+//    Column {
+//        TaskLine(Tasks(R.string.Task1))
+//        TaskLine(Tasks(R.string.Task2))
+//        TaskLine(Tasks(R.string.Task3))
+//    }
+//}
